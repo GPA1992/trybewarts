149f34b55a7cf168b9a4d08a560f0765abb36832
Gabriel Proença de Araujo <gabrielproencaaraujo@gmail.com>
2022-06-20 15:23:37 -0300

    Bonus Finalizado

--- script.js
+++ script.js
@@ -31,3 +31,89 @@ function caractereCount() {
 }
 caractereCount();
 //
+function storeName() {
+  const nameValue = document.querySelector('#input-name').value;
+  const lastNameValue = document.querySelector('#input-lastname').value;
+  localStorage.setItem('name', `${nameValue} ${lastNameValue}`);
+}
+//
+function storeEmail() {
+  const emailValue = document.querySelector('#input-email').value;
+  localStorage.setItem('email', emailValue);
+}
+//
+function storeHouse() {
+  const house = document.getElementById('house').value;
+  localStorage.setItem('casa', house);
+}
+//
+function getFamily() {
+  const familyValue = document.getElementsByName('family');
+  let familyCheck = '';
+  for (let i = 0; i < familyValue.length; i += 1) {
+    if (familyValue[i].checked === true) {
+      familyCheck = familyValue[i].value;
+    }
+  }
+  localStorage.setItem('familia', familyCheck);
+}
+//
+function getContent() {
+  const content = document.getElementsByClassName('subject');
+  const contentCheck = [];
+  for (let i = 0; i < content.length; i += 1) {
+    if (content[i].checked === true) {
+      contentCheck.push(content[i].value);
+    }
+  }
+  localStorage.setItem('conteudo', contentCheck.join(', '));
+}
+//
+function getEvaluation() {
+  const evaluation = document.getElementsByName('rate');
+  let rate = '';
+  for (let i = 0; i < evaluation.length; i += 1) {
+    if (evaluation[i].checked === true) {
+      rate = evaluation[i].value;
+    }
+  }
+  localStorage.setItem('nota', rate);
+}
+//
+function getObservation() {
+  const text = document.getElementById('textarea').value;
+  localStorage.setItem('observacao', text);
+}
+//
+function newForm() {
+  const form = document.getElementById('evaluation-form');
+  form.innerHTML ='';
+  const mainForm = document.getElementById('form');
+  const formNew = document.createElement('form');
+  formNew.id = 'form-data';
+  mainForm.appendChild(formNew);
+}
+function formItemName() {
+  const form = document.querySelector('#form-data');
+  const nome = document.createElement('div');
+  nome.innerText = `Nome: ${localStorage.getItem('name')}
+  Email: ${localStorage.getItem('email')}
+  Casa: ${localStorage.getItem('casa')}
+  Família: ${localStorage.getItem('familia')}
+  Matérias: ${localStorage.getItem('conteudo')}
+  Avaliação: ${localStorage.getItem('nota')}
+  Observações: ${localStorage.getItem('observacao')} `;
+  form.appendChild(nome);
+}
+document.querySelector('#submit-btn').addEventListener('click', () => {
+  localStorage.clear();
+  storeName();
+  storeEmail();
+  storeHouse();
+  getFamily();
+  getContent();
+  getEvaluation();
+  getObservation();
+  newForm();
+  formItemName();
+});
